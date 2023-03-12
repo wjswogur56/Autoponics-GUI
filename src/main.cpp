c89f4929f65cbb507bdf0a32a06981c34797df41
Emil Zublin <80178962+limenilbuz@users.noreply.github.com>
2023-03-12 13:37:36 -0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -395,7 +395,7 @@ static void task_update_values(lv_task_t *task) {
   lv_label_set_text_fmt(pH_text_target_val, "%.1f" , THRESHOLD.target_ph);
   lv_label_set_text_fmt(EC_text_target_val, "%.1fS" , THRESHOLD.target_ec);
   lv_label_set_text_fmt(temp_text_val, "%.1f°C" , system_measurements.temp);
-  lv_label_set_text(WL_text_val, (system_measurements.wl ? "OK" : "LOW");
+  lv_label_set_text(WL_text_val, (system_measurements.wl ? "OK" : "LOW"));
   
 }
 
